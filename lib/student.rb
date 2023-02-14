2747b8701f63954253e58acc726bca357dbff6d5
Elizabeth <mwendemuthusi6@gmail.com>
2023-02-14 07:03:06 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+require_relative './user'
+class Student < User
+  
+  def initialize (knowledge=[])
+    @knowledge=knowledge
+  end
+  def learn (str)
+    @knowledge<<str
+  end
+  def knowledge
+    @knowledge
+  end
 
 end
+s1= Student.new
+s1.last_name ="Mimi"
+p s1.knowledge
+s1.learn("Math")
+p s1.knowledge
